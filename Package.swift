1969b361da4c847a111f165e81862df1f2fdb498
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-07-25 12:33:45 +1000

    No need for Shared to be dynamic

--- Package.swift
+++ Package.swift
@@ -13,9 +13,7 @@ let package = Package(
     .Package(url: "https://github.com/PureSwift/CSDL2.git", majorVersion: 1)
   ]
 )
-
-let libShared = Product(name: "Shared", type: .Library(.Dynamic), modules: "Shared")
 let libGameEngine = Product(name: "GameEngine", type: .Library(.Dynamic), modules: "Engine")
 
-products.append(contentsOf: [libShared, libGameEngine])
+products.append(libGameEngine)
 
